93b30c5edb91ba702b5f35fa7e30c9ee7322018c
feldale <48842248+feldale@users.noreply.github.com>
2020-08-07 14:14:36 -0400

    Updated remote, now gives you qr code instead of typing it into phone

--- shitty_server/app.py
+++ shitty_server/app.py
@@ -3,18 +3,36 @@
 @author: Felipe Dale Figeman
 
 """
-
 import keyboard
 import flask
 from flask import Flask, request, redirect, render_template
 import requests
 import time
-
+import pyqrcode
+import io
 #download ngrok from https://ngrok.com/download
 app = Flask(__name__)
-oorl = ''
+
+def geturl():
+    resp = requests.get('http://127.0.0.1:4040/api/tunnels')
+    obj = resp.json()
+    pic = io.BytesIO()
+    for i in obj['tunnels']:
+        if ( not ('https' in i['public_url'])):
+            global oorl
+            oorl = i['public_url']
+            print(oorl)
+            urlqr = pyqrcode.create(oorl)
+            urlqr.png('code.png', scale=8, module_color=[0,0,0,128], background=[0xff, 0xff, 0xcc])
+            urlqr.show()
+            return oorl
+            #urlqr.svg('qrurl.svg', scale=8)
+
+oorl = geturl()
+
 @app.route('/notatvremote/', methods = ['GET', 'POST', 'DELETE'])
 def showstuff():
+    global oorl
     if (request.method == 'GET'):
     	return render_template('example.html')
     if (request.method == 'POST'):
@@ -25,23 +43,15 @@ def showstuff():
         keyboard.press_and_release(key)
         rstr = oorl + '/notatvremote/' #+ text + '/'
         return redirect(rstr, code=302)
-
     else:
         return "I have no idea what you're trying to do"
-def geturl():
-    resp = requests.get('http://127.0.0.1:4040/api/tunnels')
-    obj = resp.json()
-    for i in obj['tunnels']:
-        if ( not ('https' in i['public_url'])):
-            oorl = i['public_url']
-            print(oorl)
+
+
 
 @app.route('/')
 def rut():
     if not oorl: geturl()
     return redirect((oorl + '/notatvremote'), code=302)
-
-
-            
+        
 if __name__=='__main__':
     app.run()
